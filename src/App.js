1bd8b387842af7eebba824c75d786844ed2a39f9
Anand <anandkr19.16@gmail.com>
2020-01-30 23:32:13 +0530

    Improved UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import { Grid } from "@material-ui/core";
-
 import { SearchBar, VideoList, VideoDetail } from "./components";
-
 import youtube from "./api/youtube";
+import "./css/style.css";
 
 export default () => {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
 
   return (
+    <div className="container">
     <Grid style={{ justifyContent: "center" }} container spacing={10}>
+      <h1>Enter a word to search form Youtube</h1>
       <Grid item xs={11}>
         <Grid container spacing={10}>
           <Grid item xs={12}>
@@ -25,6 +26,7 @@ export default () => {
         </Grid>
       </Grid>
     </Grid>
+    </div>
   );
 
   async function handleSubmit(searchTerm) {
@@ -32,7 +34,7 @@ export default () => {
       params: {
         part: "snippet",
         maxResults: 5,
-        key: 'Your_API_Key',
+        key: 'your_api_key',
         q: searchTerm,
       }
     });
